6f55469e1c1d7553aad2c88fe47979b6bd52c7a4
Mangula Dube <manjula.dube@bookmyshow.com>
2016-10-18 13:08:56 +0530

    first inital commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+           <h1>This is the first sample project </h1>
+           <p>Go ahead and check </p>
+           <h3>Hello,Please feel free to contact </h3>
+         </div>
+      );
+   }
+}
+
+export default App;
